be26a3546b827cd67a95f740b9ccf0477a452a2f
Nick Payne <nick@nickpayne.net>
2016-07-03 12:22:46 +1000

    0.29 IMPROVEMENT Names of venues and songs automatically capitalise

--- app/set-list-app.module.js
+++ app/set-list-app.module.js
@@ -26,7 +26,7 @@
       'ui.bootstrap.datetimepicker',
       'ui.router'
     ])
-    .constant('VERSION', '0.28')
+    .constant('VERSION', '0.29')
     .constant('PATCH', '0')
     .config(config)
     .run(run);
